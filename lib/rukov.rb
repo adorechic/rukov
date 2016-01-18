3441fdc9c2e34daa235423870fd676fafbe1631c
adorechic <adorechic@gmail.com>
2016-01-18 21:50:33 +0900

    Parse input

--- lib/rukov.rb
+++ lib/rukov.rb
@@ -1,9 +1,17 @@
 require "rukov/version"
+require "natto"
 
 module Rukov
   class << self
     def talk(text)
-      text
+      nm = Natto::MeCab.new(rcfile: rcfile)
+      nm.parse(text) do |n|
+        p n
+      end
+    end
+
+    def rcfile
+      ENV['MECABRC'] || '/usr/local/etc/mecabrc'
     end
   end
 end
