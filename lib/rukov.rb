9753e425be40b675068585098d5a5736b6e38b56
adorechic <adorechic@gmail.com>
2016-01-19 23:04:41 +0900

    Make markov dictionary hash

--- lib/rukov.rb
+++ lib/rukov.rb
@@ -5,9 +5,24 @@ module Rukov
   class << self
     def talk(text)
       nm = Natto::MeCab.new(rcfile: rcfile)
+      nodes = []
       nm.parse(text) do |n|
-        p n
+        nodes << n
       end
+
+      hash = {}
+      nodes.each.with_index do |prefix1, index|
+        prefix2, suffix = nodes[index + 1], nodes[index + 2]
+
+        if prefix2 && suffix
+          hash[prefix1.surface] ||= {}
+          hash[prefix1.surface][prefix2.surface] ||= []
+          hash[prefix1.surface][prefix2.surface] << suffix.surface
+          hash[prefix1.surface][prefix2.surface].uniq!
+        end
+      end
+
+      hash
     end
 
     def rcfile
