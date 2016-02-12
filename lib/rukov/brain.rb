124e950ba61b994e64515b42fc3efaddf9c2ecf7
adorechic <adorechic@gmail.com>
2016-02-12 23:52:07 +0900

    Refactor

--- lib/rukov/brain.rb
+++ lib/rukov/brain.rb
@@ -2,15 +2,13 @@ module Rukov
   class Brain
     attr_accessor :start_words, :dictionary
 
-    END_CHARS = %w(。)
-
     def initialize
       @dictionary = {}
       @start_words = []
     end
 
     def memory(prefix1, prefix2, suffix)
-      suffixes(prefix1, prefix2) << end_char?(suffix) ? "" : suffix
+      suffixes(prefix1, prefix2) << WordSet.end_char?(suffix) ? "" : suffix
       suffixes(prefix1, prefix2).uniq!
     end
 
@@ -30,10 +28,6 @@ module Rukov
     end
 
     private
-
-    def end_char?(char)
-      END_CHARS.include?(char)
-    end
 
     def suffixes(prefix1, prefix2)
       dictionary[prefix1] ||= {}
--- lib/rukov/word_set.rb
+++ lib/rukov/word_set.rb
@@ -2,6 +2,12 @@ require "natto"
 
 module Rukov
   class WordSet
+    END_CHARS = %w(。)
+
+    def self.end_char?(char)
+      END_CHARS.include?(char)
+    end
+
     def initialize(text)
       nm = Natto::MeCab.new(rcfile: rcfile)
       @nodes = []
@@ -13,14 +19,14 @@ module Rukov
     def each_set
       started = true
       @nodes.each.with_index do |prefix1, index|
-        if prefix1.surface == "。"
+        if self.class.end_char?(prefix1.surface)
           started = true
           next
         end
         prefix2, suffix = @nodes[index + 1], @nodes[index + 2]
 
         if prefix2 && suffix
-          next if prefix2.surface == "。"
+          next if self.class.end_char?(prefix2.surface)
 
           yield prefix1, prefix2, suffix, started
 
